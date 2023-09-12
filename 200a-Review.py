82e16c67b52a2d11514bb0f5ada4587c48b45186
Minjik06 <minjik525@deltalearns.ca>
2023-09-12 12:35:17 -0700

    few

--- 200a-Review.py
+++ 200a-Review.py
@@ -8,7 +8,7 @@ def getIntegers(myList):
     for i in range(len(myList)):
         if myList[i]-int(myList[i])==0:
             integers.append(myList[i])
-
+    print (integers)
     return integers
 
 def getFactor(myList,number):
@@ -17,10 +17,11 @@ def getFactor(myList,number):
     # iterate through the list and add the number to the list if
     # it is a factor of the number
     factors = []
-    for i in range(len(myList)):
-        if number/myList[i]==0:
-            factors.append(myList[i])
-
+    for i in range(myList+1):
+        if i!=0:
+            if number%i==0:
+                factors.append(i)
+    print (factors)
     return factors
 
 def getNegatives(myList):
@@ -30,6 +31,7 @@ def getNegatives(myList):
     for i in range(len(myList)):
         if myList[i]<0:
             negatives.append(myList[i])
+    print(negatives)
     return negatives
 
 def getIntersection(list1,list2):
@@ -42,7 +44,7 @@ def getIntersection(list1,list2):
         for j in range(len(list2)):
             if list1[i]==list2[j]:
                 common.append(list1[i])
-
+    print(common)
     return common
 
 def getUnion(list1,list2):
@@ -54,12 +56,10 @@ def getUnion(list1,list2):
     for i in range(len(list1)):
         union.append(list1[i])
     for j in range(len(list2)):
-        union.append(list1[j])
-    union=union.sort()
-    for k in range(len(union-1)):
-        if union[k]==union[k+1]:
-            del union[k+1]
-
+        if list2[j] not in list1:
+            union.append(list2[j])
+    union.sort()
+    print(union)
     return union   
 
 def getMerge(list1,list2):
@@ -68,31 +68,32 @@ def getMerge(list1,list2):
     # add the elements of list2 into list1
     # if the list2 element is in list1, add it at the position where it occurs in list1
     # if the list2 element is not in list1, add it to the end
-    i=0
-    j=0
-    while i < len(list1):
-        while j < len(list2):
-            if list1[i]==list2[j]:
-                list1.insert(i+1,list1[i])
-                list2.pop(j)
-            j+=1
-        i+=1
-                
-    return list1
+    list3=[]
+    for i in range(len(list1)):
+        if list1[i] in list2:
+            list3.append(list1[i])
+            list3.append(list1[i])
+        else:
+            list3.append(list1[i])
+    for j in range(len(list2)):
+        if list2[j] not in list1:
+            list3.append(list2[j])
+    print(list3)
+    return list3
 
 
 def main():
-    easy1 = [5,10,15,2,4,6,8]
+    easy1 = [5, 10, 15, 2, 4, 6, 8]
     easy2 = [-2,-4,-6,2,4,6,0.1]
-    numbers1 = [3,5,8,12,11,19,10,7,2,15,25,34,16,32,50,60,100,-3,0.25]
-    numbers2 = [3,7,11,15,19,23,27,31,35,39,44,50]
+    numbers1 = [3, 5, 8, 12 ,11, 19, 10, 7, 2, 15, 25, 34, 16, 32, 50, 60, 100, -3, 0.25]
+    numbers2 = [3, 7, 11, 15, 19, 23, 27, 31, 35, 39, 44, 50]
     try:
-        assert getIntegers([3,4,1.2,1.3,5]) == [3,4,5]
-        assert getFactor(range(10),12) == [1,2,3,4,6]
-        assert getNegatives([-3,-1,0,1,4]) == [-3,-1]
+        assert getIntegers([3,4,1.2,1.3,5]) == [3, 4, 5]
+        assert getFactor(10,12) == [1, 2, 3, 4, 6]
+        assert getNegatives([-3,-1,0,1,4]) == [-3, -1]
         assert getUnion(easy1,easy2) == [-6, -4, -2, 0.1, 2, 4, 5, 6, 8, 10, 15]
-        assert getIntersection(easy1,easy2) == [2,4,6]
-        assert getMerge(easy1,easy2) == [5,10,15,2,2,4,4,6,6,8,-2,-4,-6,0.1]
+        assert getIntersection(easy1,easy2) == [2, 4, 6]
+        assert getMerge(easy1,easy2) == [5, 10, 15, 2, 2, 4, 4, 6, 6, 8, -2, -4, -6, 0.1]
         print("All assertions passed")
     except:
         print("At least 1 assertion failed")
